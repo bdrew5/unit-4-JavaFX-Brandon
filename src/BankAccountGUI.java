ec699d309d8c919125b972b4b39e548b78f97712
Brandon Drew <drewb@wlhs.wlwv.k12.or.us>
2019-11-19 07:37:36 -0800

    Update BankAccountGUI.java

--- src/BankAccountGUI.java
+++ src/BankAccountGUI.java
@@ -9,6 +9,8 @@ import javafx.scene.text.Font;
 public class BankAccountGUI extends Application {
 
     private TextField pinNum;
+    private TextField password;
+    private TextField name;
     private double money = 0;
 
     @Override
@@ -22,14 +24,39 @@ public class BankAccountGUI extends Application {
         pinNum.setAlignment(Pos.CENTER);
         pinNum.setOnAction(this::processPin);
 
+        password = new TextField();
+        password.setFont(font);
+        password.setPrefWidth(70);
+        password.setAlignment(Pos.CENTER);
+        password.setOnAction(this::processPassword);
+
+        name = new TextField();
+        name.setFont(font);
+        name.setPrefWidth(70);
+        name.setAlignment(Pos.CENTER);
+        name.setOnAction(this::processName);
+
+        Button create = new Button("Create Account");
+        create.setOnAction(this::processCreate);
+
     }
 
-    public void processPin(ActionEvent event) {
-        double r = Double.parseDouble(pinNum.getText());
-
-
-
+    public void processPin(ActionEvent event1) {
+        double pi = Double.parseDouble(pinNum.getText());
         }
+
+    public void processPassword(ActionEvent event2) {
+            double pa = Double.parseDouble(pinNum.getText());
+        }
+
+    public void processName(ActionEvent event3) {
+                double na = Double.parseDouble(pinNum.getText());
+        }
+
+    private void processIncrement(ActionEvent event4){
+            return " ";
+          }
+
         public static void main (String[]args){
             launch(args);
         }
